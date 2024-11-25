7b9813d680dedf8d260096821d024b703b46419e
Giovanny Fialho <giovannyfialho@MacBook-Air-de-Giovanny.local>
2024-11-25 17:15:12 -0300

    criacao do componente Summary

---
+++ src/components/Summary/index.tsx
@@ -0,0 +1,40 @@
+import { SummaryCard, SummaryContainer } from "@/components/Summary/styles";
+import { ArrowCircleDown, ArrowCircleUp, CurrencyDollar } from "phosphor-react";
+
+import { defaultTheme } from "@/styles/themes/default";
+
+export function Summary() {
+  return (
+    <SummaryContainer>
+      <SummaryCard>
+        <header>
+          <span>Entradas</span>
+
+          <ArrowCircleUp size={32} color={defaultTheme["green-300"]} />
+        </header>
+
+        <strong>R$ 17.400,00</strong>
+      </SummaryCard>
+
+      <SummaryCard>
+        <header>
+          <span>Saídas</span>
+
+          <ArrowCircleDown size={32} color={defaultTheme["red-300"]} />
+        </header>
+
+        <strong>R$ 17.400,00</strong>
+      </SummaryCard>
+
+      <SummaryCard variant="green">
+        <header>
+          <span>Total</span>
+
+          <CurrencyDollar size={32} color={defaultTheme.white} />
+        </header>
+
+        <strong>R$ 17.400,00</strong>
+      </SummaryCard>
+    </SummaryContainer>
+  );
+}
